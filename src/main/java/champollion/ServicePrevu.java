5ca938827a7aaec9ea79994ce0afa5e1c6701576
AchecheAchraf <98983118+AchecheAchraf@users.noreply.github.com>
2023-10-23 17:51:37 +0200

    commit

--- src/main/java/champollion/ServicePrevu.java
+++ src/main/java/champollion/ServicePrevu.java
@@ -1,6 +1,44 @@
 package champollion;
 
 public class ServicePrevu {
-	// TODO : implémenter cette classe
+	private int volumeCM;
+	private int volumeTD;
+	private int volumeTP;
+	private UE ue;
+
+	public ServicePrevu(int a,int b,int c,UE ue){
+		this.volumeCM=a;
+		this.volumeTD=b;
+		this.volumeTP=c;
+		this.ue=ue;
+	}
+
+	public int getVolumeCM() {
+        return volumeCM;
+    }
+
+    public int getVolumeTD() {
+        return volumeTD;
+    }
+
+    public int getVolumeTP() {
+        return volumeTP;
+    }
+
+    public UE getUe() {
+        return ue;
+    }
+
+    public void setVolumeCM(int volumeCM) {
+        this.volumeCM = volumeCM;
+    }
+
+    public void setVolumeTD(int volumeTD) {
+        this.volumeTD = volumeTD;
+    }
+
+    public void setVolumeTP(int volumeTP) {
+        this.volumeTP = volumeTP;
+    }
 
 }
